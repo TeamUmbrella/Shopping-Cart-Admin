0400db080fd225fa8e3fd01bf5a700382e86dc91
Edison Calderon <edison.calderon@provedatos.com>
2017-04-04 17:00:10 -0500

    modificar clientes

--- application/modules/admin/controllers/pages/clientes.php
+++ application/modules/admin/controllers/pages/clientes.php
@@ -26,25 +26,36 @@ class Clientes extends MX_Controller
       $crud->unset_jquery();
       $crud->unset_jquery_ui();
       
+      $crud->set_subject('CLIENTE');
       $crud->set_theme('flexigrid');
       $crud->set_table('CC_Clientes');
+      $crud->unset_columns(array('cli_password'));
 
-      $crud->display_as('cli_identificacion','Número de identificación');
-      $crud->required_fields('cli_identificacion', 'cli_nombre');
+      $crud->display_as('cli_identificacion', 'N. IDENTIFICACION')
+        ->display_as('cli_nombre', 'NOMBRES')
+        ->display_as('gen_codigo_genero', 'GENERO')
+        ->display_as('cli_direccion', 'DIRECCION')
+        ->display_as('cli_telefono', 'TELEFONO')
+        ->display_as('cli_email', 'E-MAIL')
+        ->display_as('cli_imagen', 'IMAGEN')
+        ->display_as('cli_password', 'PASSWORD')
+        ->display_as('cli_fecha_nacimiento', 'FECHA NACIMIENTO');
+      $crud->required_fields('cli_identificacion', 'cli_nombre','cli_fecha_nacimiento');
       $crud->unique_fields('cli_identificacion');
-      $crud->set_field_upload('cli_direccion','assets/admin/uploads/main');
-      $crud->set_relation('gen_codigo_genero','CC_Generos','gen_nombre_genero');
-      /*$crud->set_subject('');
-      $crud->field_type('tiene_boton', 'dropdown', array('1' => 'SI', '0' => 'NO'));
-      $crud->columns('titulo', 'subtitulo', 'orden', 'imagen');
-
+      $crud->set_field_upload('cli_imagen','assets/admin/uploads/main');
+      $crud->set_relation('gen_codigo_genero', 'CC_Generos', 'gen_nombre_genero');
+      $crud->edit_fields('cli_identificacion', 'cli_nombre', 'gen_codigo_genero', 'cli_fecha_nacimiento', 'cli_direccion', 'cli_telefono', 'cli_email', 'cli_imagen', 'cli_password');
+      $crud->field_type('cli_password', 'password');
+      $crud->unset_read();
+      /*
       $crud->unset_export();
       $crud->unset_print();
-      $crud->unset_read();*/
+      $crud->unset_read();
+      */
       
       $output = $crud->render();
       
-      $data['title_for_layout'] = "CLIENTES";
+      $data['title_for_layout'] = "LISTADO DE CLIENTES";
       $data['grocery'] = true;
       $data['grocery_crud'] = $output;
       $this->layout->view('pages/Clientes', $data);
